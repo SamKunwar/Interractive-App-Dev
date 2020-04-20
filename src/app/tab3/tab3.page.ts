57ee024294127ce3b978b0c74a58ea0d2cdc98ba
Samratkunwar <samratkunwar12@gmail.com>
2020-04-20 12:00:22 +1000

    complete

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { async } from '@angular/core/testing';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-tab3',
@@ -9,13 +11,34 @@ import { Storage } from '@ionic/storage';
 export class Tab3Page {
 
   remName: string;
-  remDate: Date;
-  toggleBtn: boolean;
+  remDate: string;
+  toggleBtn: boolean ;
+  notfName: string;
+  notfDate: string;
 
-  constructor(private storage: Storage) {}
+  constructor(private storage: Storage, private router: Router) {}
 
-  store() {
-    alert("pressed");
+  async ngOnInit() {
+
+    this.toggleBtn = false;
+    this.remDate = new Date().toISOString();
+
+    this.notfName = await this.storage.get("Reminder Name");
+    this.notfDate = await this.storage.get("Reminder Date");
+
+    if (this.notfName == null) this.notfName = "";
+    if (this.notfDate == null) this.notfDate = this.remDate;
+
+    this.remName = this.notfName;
+    this.remDate = this.notfDate;
+
+  }
+
+
+
+  async store() {
+    this.storage.set("Reminder Name", this.remName);
+    this.storage.set("Reminder Date", this.remDate); 
   }
 
 }
