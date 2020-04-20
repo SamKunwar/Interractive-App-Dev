31cc39732f6f6f88ce810c1595de6119db02f35b
Samratkunwar <samratkunwar12@gmail.com>
2020-04-20 10:30:07 +1000

    storage added

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  remName: string;
+  remDate: Date;
+  toggleBtn: boolean;
+
+  constructor(private storage: Storage) {}
+
+  store() {
+    alert("pressed");
+  }
 
 }
